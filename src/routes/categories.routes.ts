9157433c49c3dad74c5b47394b6d6b4900f09a4d
WagnerReis <wagnerreis1997@gmail.com>
2022-05-19 09:40:23 -0300

    feat: usando docker compose

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
